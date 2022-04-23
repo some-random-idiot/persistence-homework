debd664fc7b00f8f284992bada5dbf950fc351b6
some random idiot <sittipat2554aa@hotmail.com>
2022-04-23 17:41:30 +0700

    Gave DaoFactory the responsibility of managing the connection.

--- src/persistence/dao/DaoFactory.java
+++ src/persistence/dao/DaoFactory.java
@@ -1,14 +1,17 @@
 package persistence.dao;
 
-import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DaoFactory {
-    private final ConnectionSource connectionSource;
+    private final JdbcConnectionSource connectionSource;
 
-    public DaoFactory(ConnectionSource connectionSource) {
-        this.connectionSource = connectionSource;
+    public DaoFactory() throws IOException, SQLException {
+        this.connectionSource = createConnection();
     }
 
     public DaoSkeleton getDao(String type) throws SQLException {
@@ -19,5 +22,13 @@ public class DaoFactory {
             return new DeathDao(connectionSource);
         }
         return null;
+    }
+
+    private static JdbcConnectionSource createConnection() throws IOException, SQLException {
+        FileReader reader = new FileReader("././database.properties");
+        Properties properties = new Properties();
+        properties.load(reader);
+        String DB_PATH = properties.getProperty("DB_PATH");
+        return new JdbcConnectionSource("jdbc:sqlite:" + DB_PATH);
     }
 }
