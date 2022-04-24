8cb450fb8e679e09387299d30338754af2cd3ca1
some random idiot <sittipat2554aa@hotmail.com>
2022-04-24 12:50:02 +0700

    Refactored code to be less repetitive.

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,13 +1,13 @@
 package gui;
 
-import com.j256.ormlite.stmt.query.In;
-import persistence.dao.*;
-import persistence.entity.*;
+import persistence.dao.BirthDao;
+import persistence.dao.DaoFactory;
+import persistence.dao.DeathDao;
+import persistence.entity.BirthRecord;
+import persistence.entity.DeathRecord;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -29,72 +29,45 @@ public class Main extends JFrame {
     private void setupButtons() {
         JButton birthsButton = new JButton("Show Birth Records");
         birthsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        birthsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    showBirths();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+        birthsButton.addActionListener(e -> {
+            try {
+                showBirths();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
-            }
+        }
         );
 
         JButton deathsButton = new JButton("Show Death Records");
         deathsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        deathsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    showDeaths();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
+        deathsButton.addActionListener(e -> {
+            try {
+                showDeaths();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
-            }
+        }
         );
 
         JButton addUpdateBirth = new JButton("Add or Update a Birth Record");
         addUpdateBirth.setAlignmentX(Component.CENTER_ALIGNMENT);
-        addUpdateBirth.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
+        addUpdateBirth.addActionListener(e -> {
                     createOrUpdateBirthRec();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
                 }
-            }
-            }
         );
 
         JButton addUpdateDeath = new JButton("Add or Update a Death Record");
         addUpdateDeath.setAlignmentX(Component.CENTER_ALIGNMENT);
-        addUpdateDeath.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
+        addUpdateDeath.addActionListener(e -> {
                     createOrUpdateDeathRec();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
                 }
-            }
-            }
         );
 
         JButton findButton = new JButton("Find a Record");
         findButton.setAlignmentX(Component.CENTER_ALIGNMENT);
-        findButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
+        findButton.addActionListener(e -> {
                     findRecord();
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
                 }
-            }
-            }
         );
 
         add(Box.createVerticalGlue());
@@ -115,18 +88,14 @@ public class Main extends JFrame {
         // Create a new window to show the birth records.
         List<BirthRecord> birthRecords = birthDao.queryForAll();
         JFrame window = new JFrame("Birth Records");
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-        JScrollPane scrollPane = new JScrollPane(container);
-        window.setSize(400, 400);
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
+        JPanel container = getJPanel(window);
         for (BirthRecord birthRecord : birthRecords) {
             JLabel label = new JLabel(birthRecord.getPeriod() + " " + birthRecord.getRegion() + " "
                                       + birthRecord.getBirth_count());
             container.add(label);
         }
+
+        JScrollPane scrollPane = new JScrollPane(container);
         window.add(scrollPane);
         window.setVisible(true);
     }
@@ -135,31 +104,22 @@ public class Main extends JFrame {
         // Create a new window to show the death records.
         List<DeathRecord> deathRecords = deathDao.queryForAll();
         JFrame window = new JFrame("Death Records");
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-        JScrollPane scrollPane = new JScrollPane(container);
-        window.setSize(400, 400);
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
+        JPanel container = getJPanel(window);
         for (DeathRecord deathRecord : deathRecords) {
             JLabel label = new JLabel(deathRecord.getPeriod() + " " + deathRecord.getRegion() + " "
                     + deathRecord.getDeath_count());
             container.add(label);
         }
+
+        JScrollPane scrollPane = new JScrollPane(container);
         window.add(scrollPane);
         window.setVisible(true);
     }
 
-    private void createOrUpdateBirthRec() throws SQLException {
+    private void createOrUpdateBirthRec() {
         // Update a birth record or create a new one.
         JFrame window = new JFrame("Add/Update a Birth Record");
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-        window.setSize(400, 400);
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
+        JPanel container = getJPanel(window);
         JLabel periodLabel = new JLabel("Period:");
         JLabel regionLabel = new JLabel("Region:");
         JLabel birthsLabel = new JLabel("Births:");
@@ -167,22 +127,19 @@ public class Main extends JFrame {
         JTextField regionField = new JTextField(10);
         JTextField birthsField = new JTextField(10);
         JButton addOrUpdateButton = new JButton("Add/Update");
-        addOrUpdateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int period = Integer.parseInt(periodField.getText());
-                    String region = regionField.getText();
-                    int births = Integer.parseInt(birthsField.getText());
-                    BirthRecord birthRecord = new BirthRecord(period, region, births);
-                    assert birthDao != null;
-                    birthDao.createOrUpdateRec(birthRecord);
-                    window.dispose();
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+        addOrUpdateButton.addActionListener(e -> {
+            try {
+                int period = Integer.parseInt(periodField.getText());
+                String region = regionField.getText();
+                int births = Integer.parseInt(birthsField.getText());
+                BirthRecord birthRecord = new BirthRecord(period, region, births);
+                assert birthDao != null;
+                birthDao.createOrUpdateRec(birthRecord);
+                window.dispose();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
             }
-            }
+        }
         );
         container.add(periodLabel);
         container.add(periodField);
@@ -195,15 +152,10 @@ public class Main extends JFrame {
         window.setVisible(true);
     }
 
-    private void createOrUpdateDeathRec() throws SQLException {
+    private void createOrUpdateDeathRec() {
         // Update a death record or create a new one.
         JFrame window = new JFrame("Add/Update a Death Record");
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-        window.setSize(400, 400);
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
+        JPanel container = getJPanel(window);
         JLabel periodLabel = new JLabel("Period:");
         JLabel regionLabel = new JLabel("Region:");
         JLabel deathsLabel = new JLabel("Deaths:");
@@ -211,22 +163,19 @@ public class Main extends JFrame {
         JTextField regionField = new JTextField(10);
         JTextField deathsField = new JTextField(10);
         JButton addOrUpdateButton = new JButton("Add/Update");
-        addOrUpdateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int period = Integer.parseInt(periodField.getText());
-                    String region = regionField.getText();
-                    int deaths = Integer.parseInt(deathsField.getText());
-                    DeathRecord deathRecord = new DeathRecord(period, region, deaths);
-                    assert deathDao != null;
-                    deathDao.createOrUpdateRec(deathRecord);
-                    window.dispose();
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
-                }
+        addOrUpdateButton.addActionListener(e -> {
+            try {
+                int period = Integer.parseInt(periodField.getText());
+                String region = regionField.getText();
+                int deaths = Integer.parseInt(deathsField.getText());
+                DeathRecord deathRecord = new DeathRecord(period, region, deaths);
+                assert deathDao != null;
+                deathDao.createOrUpdateRec(deathRecord);
+                window.dispose();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
             }
-            }
+        }
         );
         container.add(periodLabel);
         container.add(periodField);
@@ -239,40 +188,32 @@ public class Main extends JFrame {
         window.setVisible(true);
     }
 
-    private void findRecord() throws SQLException {
+    private void findRecord() {
         // Find a record.
         JFrame window = new JFrame("Find a Record");
-        JPanel container = new JPanel();
-        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-        window.setSize(400, 400);
-        window.setResizable(false);
-        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
+        JPanel container = getJPanel(window);
         JLabel periodLabel = new JLabel("Period:");
         JLabel regionLabel = new JLabel("Region:");
         JTextField periodField = new JTextField(10);
         JTextField regionField = new JTextField(10);
         JButton findButton = new JButton("Find");
-        findButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    int period = Integer.parseInt(periodField.getText());
-                    String region = regionField.getText();
-                    assert birthDao != null;
-                    BirthRecord birthRecord = birthDao.findByPeriodAndRegion(period, region);
-                    assert deathDao != null;
-                    DeathRecord deathRecord = deathDao.findByPeriodAndRegion(period, region);
-                    if (birthRecord != null && deathRecord != null) {
-                        JOptionPane.showMessageDialog(window, "Birth: " + birthRecord.getBirth_count() + "\nDeath: " + deathRecord.getDeath_count());
-                    } else {
-                        JOptionPane.showMessageDialog(window, "No record found.");
-                    }
-                } catch (SQLException e1) {
-                    e1.printStackTrace();
+        findButton.addActionListener(e -> {
+            try {
+                int period = Integer.parseInt(periodField.getText());
+                String region = regionField.getText();
+                assert birthDao != null;
+                BirthRecord birthRecord = birthDao.findByPeriodAndRegion(period, region);
+                assert deathDao != null;
+                DeathRecord deathRecord = deathDao.findByPeriodAndRegion(period, region);
+                if (birthRecord != null && deathRecord != null) {
+                    JOptionPane.showMessageDialog(window, "Birth: " + birthRecord.getBirth_count() + "\nDeath: " + deathRecord.getDeath_count());
+                } else {
+                    JOptionPane.showMessageDialog(window, "No record found.");
                 }
+            } catch (SQLException e1) {
+                e1.printStackTrace();
             }
-            }
+        }
         );
         container.add(periodLabel);
         container.add(periodField);
@@ -283,11 +224,21 @@ public class Main extends JFrame {
         window.setVisible(true);
     }
 
+    private JPanel getJPanel(JFrame window) {
+        JPanel container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        window.setSize(400, 400);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        window.setLayout(new BoxLayout(window.getContentPane(), BoxLayout.Y_AXIS));
+        return container;
+    }
+
     private static void start() {
         new Main();
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         start();
     }
 }
