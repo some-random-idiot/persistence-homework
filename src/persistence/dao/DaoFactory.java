ad63f1ca9ec403fd1c4982bfcf3e1fb0325081ab
some random idiot <sittipat2554aa@hotmail.com>
2022-04-24 10:02:25 +0700

    Updated DaoFactory.

--- src/persistence/dao/DaoFactory.java
+++ src/persistence/dao/DaoFactory.java
@@ -3,32 +3,35 @@ package persistence.dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 
 import java.io.FileReader;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class DaoFactory {
-    private final JdbcConnectionSource connectionSource;
+    private static final JdbcConnectionSource connectionSource = createConnection();
 
-    public DaoFactory() throws IOException, SQLException {
-        this.connectionSource = createConnection();
-    }
-
-    public DaoSkeleton getDao(String type) throws SQLException {
-        if (type.equals("BirthDao")) {
-            return new BirthDao(connectionSource);
-        }
-        else if (type.equals("DeathDao")) {
-            return new DeathDao(connectionSource);
+    public static DaoSkeleton getDao(String type) {
+        try {
+            if (type.equals("BirthDao")) {
+                return new BirthDao(connectionSource);
+            } else if (type.equals("DeathDao")) {
+                return new DeathDao(connectionSource);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
-    private static JdbcConnectionSource createConnection() throws IOException, SQLException {
-        FileReader reader = new FileReader("././database.properties");
-        Properties properties = new Properties();
-        properties.load(reader);
-        String DB_PATH = properties.getProperty("DB_PATH");
-        return new JdbcConnectionSource("jdbc:sqlite:" + DB_PATH);
+    private static JdbcConnectionSource createConnection() {
+        try {
+            FileReader reader = new FileReader("././database.properties");
+            Properties properties = new Properties();
+            properties.load(reader);
+            String DB_PATH = properties.getProperty("DB_PATH");
+            return new JdbcConnectionSource("jdbc:sqlite:" + DB_PATH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
